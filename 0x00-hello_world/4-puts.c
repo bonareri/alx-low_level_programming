cab492ec655adc303c02da1e476a14f0bda2d5f1
Melody <melodybonareri123@gmail.com>
2023-02-16 05:54:20 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - Print a new line using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
