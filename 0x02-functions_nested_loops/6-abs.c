b4eb17643f134fb9291874bbd86d9e9f713f2387
Melody <melodybonareri123@gmail.com>
2023-02-21 12:51:00 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @n: the int in the question
+ *
+ * Retrn: the abs of the interger
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
