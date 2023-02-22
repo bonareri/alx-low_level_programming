ceed3bc794e19e07636eff41fb9fbae420c05ce5
Melody <melodybonareri123@gmail.com>
2023-02-22 11:51:25 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,17 +7,20 @@
 
 void jack_bauer(void)
 {
-	int min = 0;
-	int hr = 0;
+	int hr, min;
 
-	for (hr = 0; hr < 24; hr++)
+	for (hr = 0; hr < 23; hr++)
 	{
-	for (min = 0; min < 59; min++)
+		for (min = 0; min <= 59; min++)
 	{
-		_putchar("%02d:%02d", hr, min);
-		_putchar("\n");
+		_putchar(( hr / 10) + '0');
+		_putchar(( hr % 10) + '0');
+		_putchar(':');
+		_putchar(( min / 10) + '0');
+
+		_putchar(( min % 10) + '0');
+
+		_putchar('\n');
 	}
-	_putchar("%02d:%02d", hr, min);
-	_putchar("\n");
 	}
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,26 +8,22 @@
 
 void times_table(void)
 {
-	int num, mult, prod;
+	int a, b, x, mult, chek;
 
-	for (num = 0; num <= 9; num++)
+	for (a = 0; a <= 9; a++)
 	{
-		_putchar(0);
+	for (b = 0, x = 1; b < 9; b++, x++)
+	{
+	mult = a * b;
+	chek = a * x;
 
-		for (mult = 1; mult <= 9; num++)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num + mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
-		}
-	_putchar('\n');
+	if (chek > 9 && b != 9)
+		printf("%d, ", mult);
+	else if (b == 9)
+		printf("%d", mult);
+	else
+		printf("%d,  ", mult);
+	}
+	putchar('\n');
 	}
 }
